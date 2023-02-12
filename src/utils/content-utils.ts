5c0f9fa82448b28a7b4d57aae5f08c4c96a26c45
Dennis Luken <dennis@thewebsitewiz.com>
2023-02-12 18:19:15 -0500

    more run away script files

---
+++ src/utils/content-utils.ts
@@ -0,0 +1,40 @@
+const puppeteer = require("puppeteer");
+
+const UserAgents = require("./user-agents");
+
+async function getPageContent(passed: any, url: string) {
+    const browser = await puppeteer.launch({
+        headless: true,
+    });
+
+    const page = await browser.newPage();
+    const userAgent = UserAgents.getUserAgent();
+    await page.setUserAgent(userAgent.agent);
+    await page.goto(passed[url], {
+        waitUntil: "domcontentloaded",
+        timeout: 300000,
+    });
+    page.setDefaultTimeout(0);
+
+    const rawData = await page.content();
+    const jsonRegEx = new RegExp(/<pre.*?>(.*)<\/pre>/);
+
+    const result = jsonRegEx.exec(rawData);
+    let pageContent: string | null = null;
+    if (result !== null && result[1] !== null) {
+        pageContent = result[1];
+    }
+
+    let pageJson = null;
+    if (pageContent !== null) {
+        pageJson = JSON.parse(pageContent);
+    }
+
+    await browser.close();
+
+    return pageJson;
+}
+
+module.exports.getPageContent = getPageContent;
+
+
