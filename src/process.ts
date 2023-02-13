a16054bad3459cacc5d02a990460d29aa0b039b1
Dennis Luken <dennis@thewebsitewiz.com>
2023-02-13 10:22:35 -0500

    fixing mongoose schema constructor

--- src/process.ts
+++ src/process.ts
@@ -2,15 +2,39 @@
 const fs = require("fs");
 const path = require("path");
 
-const { StockSymbolInfo } = require('./interfaces/stockSymbolInfo');
-const { StockSymbol } = require('./schemas/stockSymbols');
+const StockSymbolInfo = require('./interfaces/stockSymbolInfo');
+const StockSymbol = require('./schemas/stockSymbols');
 
 const loopUtils = require("./utils/loop-utils");
 const contentUtils = require("./utils/content-utils");
+const mongoUtils = require("./utils/mongodb-utils");
 
 const companyProfileUrlTpl = "https://api.nasdaq.com/api/company/__STOCK_SYMBOL__/company-profile";
 
+const stockInsertInfo = [
+    "symbol",
+    "companyName",
+    "stockType",
+    "exchange",
+    "isNasdaqListed",
+    "isNasdaq100",
+    "isHeld",
+    "assetClass",
+    "keyStats",
+    "notifications"
+];
 
+const stockInsertSummary = [
+    "AddressString",
+    "Address",
+    "Phone",
+    "Industry",
+    "Sector",
+    "Region",
+    "CompanyUrl",
+    "CompanyDescription",
+    "KeyExecutives"
+];
 // loopUtils.loopThruStocks(insertStockSymbol);
 loopUtils.loopThruStocks(getStockDataForInsert);
 
@@ -22,7 +46,6 @@ async function getStockDataForInsert(exchange: string, stockSymbol: string) {
     const exchangePath = path.join(__dirname, "scrape-data", exchange);
     const subDir = stockSymbol.charAt(0).toUpperCase();
 
-
     const stockInfoPath = path.join(
         exchangePath,
         subDir,
@@ -32,34 +55,8 @@ async function getStockDataForInsert(exchange: string, stockSymbol: string) {
     try {
         const stockInsert: typeof StockSymbolInfo = {};
 
-        const stockInsertInfo = [
-            "symbol",
-            "companyName",
-            "stockType",
-            "exchange",
-            "isNasdaqListed",
-            "isNasdaq100",
-            "isHeld",
-            "assetClass",
-            "keyStats",
-            "notifications"
-        ];
-
-        const stockInsertSummary = [
-            "AddressString",
-            "Address",
-            "Phone",
-            "Industry",
-            "Sector",
-            "Region",
-            "CompanyUrl",
-            "CompanyDescription",
-            "KeyExecutives"
-        ];
-
         const stockJson = fs.readFileSync(stockInfoPath, 'utf8');
         if (stockJson?.data !== null) {
-
             for (let property of stockInsertInfo) {
                 stockInsert[property] = stockJson[property];
             }
@@ -71,22 +68,27 @@ async function getStockDataForInsert(exchange: string, stockSymbol: string) {
         );
 
         const summaryFilePath = path.join(exchangePath, subDir, `${stockSymbol}-summary.json`);
-        let summaryJson;
+        let summaryJson: any;
 
-        if (!fs.existsSync(summaryFilePath)) {
-            summaryJson = contentUtils.getPageContent({ summaryUrl: stockSummaryUrl }, "summaryUrl");
+        // if (!fs.existsSync(summaryFilePath)) {
+        summaryJson = await contentUtils.getPageContent({ summaryUrl: stockSummaryUrl }, "summaryUrl");
 
-            if (summaryJson?.data !== null) {
-                fs.writeFileSync(
-                    summaryFilePath,
-                    JSON.stringify(summaryJson)
-                );
+        if (summaryJson?.data !== null) {
+            fs.writeFileSync(
+                summaryFilePath,
+                JSON.stringify(summaryJson)
+            );
 
-            }
         }
+
+        // *************************
+        // process.exit(0)
+        // *************************
+
+        /* }
         else {
             summaryJson = fs.readFileSync(summaryFilePath, 'utf8');
-        }
+        } */
 
         if (summaryJson?.data !== null) {
             for (let property of stockInsertSummary) {
@@ -99,46 +101,55 @@ async function getStockDataForInsert(exchange: string, stockSymbol: string) {
                 }
 
             }
+
+            try {
+                let stock = new StockSymbol(stockInsert);
+                console.log("stock insert:", stock)
+
+            } catch (err) {
+                console.error(exchange, stockSymbol, err);
+            }
+
+            // *************************
+            process.exit(0)
+            // *************************
+
         }
-
-        let stock = new StockSymbol(stockInsert);
-
-    } catch (err) {
-        console.error(exchange, stockSymbol, err);
     }
-
-}
-
-
-
-async function insertStockSymbol(stockSymbolInfo: typeof StockSymbolInfo) {
-    try {
-        let stock = new StockSymbol({
-            "symbol": "BAER",
-            "companyName": "Bridger Aerospace Group Holdings, Inc. Common Stock",
-            "stockType": "Common Stock",
-            "exchange": "NASDAQ-GM",
-            "isNasdaqListed": true,
-            "isNasdaq100": false,
-            "isHeld": false,
-            "assetClass": "STOCKS",
-            "keyStats": null,
-            "notifications": [],
-            "CompanyName": "Bridger Aerospace Group Holdings, Inc.",
-            "Address": "90 AVIATION LANE, BELGRADE, Montana, 59714, United States",
-            "Phone": "+1 406 813-0079",
-            "Industry": null,
-            "Sector": null,
-            "Region": "North America",
-            "CompanyDescription": "Bridger Aerospace Group Holdings Inc operates as an aerial firefighting company. It provides aerial wildfire surveillance, relief and suppression, and aerial firefighting services using next-generation technology and environmentally friendly and sustainable firefighting methods. Bridger Aerospace provides aerial firefighting and wildfire management services to federal and state government agencies, including the United States Forest Service, across the nation.",
-            "CompanyUrl": "www.bridgeraerospace.com",
-            "KeyExecutives": []
-        });
-
-        stock = await stock.save();
-
-    }
-    catch (e) {
-
+    catch (err) {
+        console.error("outer try block", err);
     }
 }
+/* 
+        async function insertStockSymbol(stockSymbolInfo: typeof StockSymbolInfo) {
+            try {
+                let stock = new StockSymbol({
+                    "symbol": "BAER",
+                    "companyName": "Bridger Aerospace Group Holdings, Inc. Common Stock",
+                    "stockType": "Common Stock",
+                    "exchange": "NASDAQ-GM",
+                    "isNasdaqListed": true,
+                    "isNasdaq100": false,
+                    "isHeld": false,
+                    "assetClass": "STOCKS",
+                    "keyStats": null,
+                    "notifications": [],
+                    "CompanyName": "Bridger Aerospace Group Holdings, Inc.",
+                    "Address": "90 AVIATION LANE, BELGRADE, Montana, 59714, United States",
+                    "Phone": "+1 406 813-0079",
+                    "Industry": null,
+                    "Sector": null,
+                    "Region": "North America",
+                    "CompanyDescription": "Bridger Aerospace Group Holdings Inc operates as an aerial firefighting company. It provides aerial wildfire surveillance, relief and suppression, and aerial firefighting services using next-generation technology and environmentally friendly and sustainable firefighting methods. Bridger Aerospace provides aerial firefighting and wildfire management services to federal and state government agencies, including the United States Forest Service, across the nation.",
+                    "CompanyUrl": "www.bridgeraerospace.com",
+                    "KeyExecutives": []
+                });
+
+                stock = await stock.save();
+
+            }
+            catch (e) {
+
+            }
+        }
+         */
--- src/utils/loop-utils.ts
+++ src/utils/loop-utils.ts
@@ -1,14 +1,23 @@
 const symbolUtils = require("./symbol-utils");
 const symbolLists = symbolUtils.getExchangeSymbols();
 
-module.exports.loopThruStocks = (doThis: Function) => {
+module.exports.loopThruStocks = async (doThis: Function) => {
+
+    let cnt = 0;
     for (let exchange in symbolLists) {
         if (symbolLists.hasOwnProperty(exchange)) {
             const stockSymbols = symbolLists[exchange];
             exchange = exchange.toLowerCase();
             for (let stockSymbol in stockSymbols) {
                 stockSymbol = stockSymbol.toLowerCase();
-                doThis(exchange, stockSymbol);
+                await doThis(exchange, stockSymbol);
+                cnt++;
+                if (cnt > 5) {
+                    // *************************
+                    process.exit(0)
+                    // *************************
+
+                }
             }
         }
     }
